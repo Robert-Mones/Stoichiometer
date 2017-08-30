78a4ec685ca28b6a420d1b6cc669a4f4eea66394
Justin Stephens <jstephens2020@outlook.com>
2017-08-30 13:24:00 -0400

    Added functions for breaking down
    Function BreakDown() will break an equation into a Vector of
    sub-equations by Element.
    
    Function IsBrokenDown() returns whether a function is in it’s minimal
    form or not

--- Equation.java
+++ Equation.java
@@ -24,6 +24,56 @@ public class Equation
         right = r;
     }
     
+    public boolean IsBrokenDown()
+    {
+        if (left.size() == 0 || right.size() == 0)
+            return false;
+        
+        int Element = left.get(0).element;
+        for (int i = 1; i < left.size(); i++)
+            if (left.get(i).element != Element)
+                return false;
+        
+        for (int i = 0; i < right.size(); i++)
+            if (right.get(i).element != Element)
+                return false;
+        
+        return true;
+    }
+    public Vector<Equation> BreakDown()
+    {
+        Vector<Integer> elements = new Vector<Integer>();
+        
+        for (int i = 0; i < left.size(); i++)
+            if (!elements.contains(left.get(i).element))
+                elements.add(left.get(i).element);
+        
+        for (int i = 0; i < right.size(); i++)
+            if (!elements.contains(right.get(i).element))
+                elements.add(right.get(i).element);
+        
+        
+        
+        Vector<Equation> equations = new Vector<Equation>();
+        for (int i = 0; i < elements.size(); i++)
+        {
+            int element = elements.get(i);
+            Equation eq = new Equation();
+            
+            
+            for (int j = 0; j < left.size(); j++)
+                if (left.get(j).element == element)
+                    eq.left.add(left.get(j));
+            
+            for (int j = 0; j < right.size(); j++)
+                if (right.get(j).element == element)
+                    eq.right.add(right.get(j));
+            
+            equations.add(eq);
+        }
+        return equations;
+    }
+    
     public void addTermLeft(Term t)
     {
         left.add(t);
