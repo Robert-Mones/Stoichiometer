a3229735963ff1668238f76f5d66fb863027b7df
Justin Stephens <jstephens2020@outlook.com>
2017-08-30 10:25:38 -0400

    Added Documentation

--- Term.java
+++ Term.java
@@ -6,7 +6,7 @@
  * Term member variables are all public to emulate a C-struct
  *
  * @author Justin Stephens
- * @version 29 August, 2017
+ * @version 30 August, 2017
  */
 public class Term
 {
@@ -15,10 +15,16 @@ public class Term
     public int variable;
     public int element;
 
-    Term()
-    {
-        this(0, 1, 0, 0);
-    }
+    /**
+     * Returns whether the term is a fraction or not (i.e. has a denominator
+     * other than 1).
+     * 
+     * @param  num  The numerator of the coefficient
+     * @param  den  The denominator of the coefficient
+     * @param  v    The variable (z, y, x, w...)
+     * @param  e    The element (A, B, C, D...)
+     * @return True when the term is a fraction, False otherwise
+     */
     Term (int num, int den, int v, int e)
     {
         numerator = num;
@@ -26,12 +32,28 @@ public class Term
         variable = v;
         element = e;
     }
+    Term()
+    {
+        this(0, 1, 0, 0);
+    }
     
+    /**
+     * Returns whether the term is a fraction or not (i.e. has a denominator
+     * other than 1).
+     *
+     * @return True when the term is a fraction
+     */
     public boolean IsFraction()
     {
         return (denominator != 1);
     }
     
+    /**
+     * Returns a human-readable representation of the Term's Contents
+     * Ex. 1/2zA
+     *
+     * @return String containing Term
+     */
     public String toString()
     {
         return numerator + ((this.IsFraction()) ? "/" + denominator : "")
